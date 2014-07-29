cd00f6f667314b4bffefdaee049effc080864a65
Anthony Arrants <antnya@fullsail.edu>
2014-07-29 19:39:02 -0400

    Working on functions

--- arrants_anthony_project4.js
+++ arrants_anthony_project4.js
@@ -1,1 +1,25 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+//
+//
+//
+//
+
+// Does a string follow a 123-456-7890 pattern?
+var phonecheck = function(string) {
+    var stringComponents = string.split('-');
+    if (stringComponents.length === 3 && stringComponents[0].length === 3 && stringComponents[1].length === 3
+        && stringComponents[2].length === 4) {
+        for(string in stringComponents) {
+          if (isNaN(stringComponents[string])) 
+            return false;
+        }
+        return true;
+    }
+    return false;
+};
+
+console.log(phonecheck("123-456-7890"));
+console.log(phonecheck("123-45l-0987"));
+console.log(phonecheck("23-456-7890"));
+console.log(phonecheck("1221123"));
