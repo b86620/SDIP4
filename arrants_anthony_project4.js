bf7424c133be75fe6d208e5b775dabbc51378a2b
Anthony Arrants <antnya@fullsail.edu>
2014-07-29 19:59:37 -0400

    Working on functions

--- arrants_anthony_project4.js
+++ arrants_anthony_project4.js
@@ -5,7 +5,9 @@
 //
 //
 
+
 // Does a string follow a 123-456-7890 pattern?
+
 var phonecheck = function(string) {
     var stringComponents = string.split('-');
     if (stringComponents.length === 3 && stringComponents[0].length === 3 && stringComponents[1].length === 3
@@ -23,3 +25,37 @@ console.log(phonecheck("123-456-7890"));
 console.log(phonecheck("123-45l-0987"));
 console.log(phonecheck("23-456-7890"));
 console.log(phonecheck("1221123"));
+
+// Does a string follow a aaa@bbb.ccc pattern like an e-mail address?
+
+var emailCheck = function(string) {
+   var emailSections = string.split ('@');
+   if(emailSections.length === 2) {
+    var addressSection = emailSections[1].split('.');
+    if (addressSection[addressSection.length-1].length > 1 && addressSection[0].length > 0 && emailSections[0].length > 0) {
+       return true;
+    }
+    return false;
+  }
+  return false;
+};
+
+console.log(emailCheck("superlink@gmail.com"));
+console.log(emailCheck("@google.com"));
+console.log(emailCheck("l@g.c"));
+console.log(emailCheck("l@g.co"));
+console.log(emailCheck("@."));
+
+// Is the string a URL?
+var urlCheck = function(url) {
+if(url.indexOf("http://") === 0 || url.indexOf("https://") === 0)
+    return true;
+  return false;
+};
+
+console.log(urlCheck("httpp://"));
+console.log(urlCheck("http://www.google.com"));
+console.log(urlCheck("https://www.ih8mileycyrus.com"));
+console.log(urlCheck("http//www.google.com"));
+
+
