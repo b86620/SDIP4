4ac4f9e73e361e5f8c4474d17273080bc4af4d07
Anthony Arrants <antnya@fullsail.edu>
2014-07-30 20:09:44 -0400

    added some more functions.

--- arrants_anthony_project4.js
+++ arrants_anthony_project4.js
@@ -8,6 +8,7 @@
 
 // Does a string follow a 123-456-7890 pattern?
 
+
 var phonecheck = function(string) {
     var stringComponents = string.split('-');
     if (stringComponents.length === 3 && stringComponents[0].length === 3 && stringComponents[1].length === 3
@@ -55,7 +56,28 @@ if(url.indexOf("http://") === 0 || url.indexOf("https://") === 0)
 
 console.log(urlCheck("httpp://"));
 console.log(urlCheck("http://www.google.com"));
-console.log(urlCheck("https://www.ih8mileycyrus.com"));
+console.log(urlCheck("https://www.this.com"));
 console.log(urlCheck("http//www.google.com"));
 
+
+// titlecase
+
+var titleCase = function(string) {
+   var words = string.split(" ");
+   var upperWords = [];
+   for(var word in words) {
+      upperWords.push(words[word].substring(0,1).toUpperCase() + words[word].substring(1));
+   }
+   return upperWords.join(" ");
+};
+
+console.log(titleCase("This is awesome"));
+
+//
+var substituteSeparator = function(string, current, revised) {
+    var separated = string.split(current);
+    return separated.join(revised);
+};
+
+console.log(substituteSeparator('a,b,c', ',', '/'));
 
